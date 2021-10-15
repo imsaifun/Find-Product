ad5dcab350f49e46dafade77787d0070ce813909
Golam Mostafa <89465189+imsaifun@users.noreply.github.com>
2021-10-15 03:58:55 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+      const inventory = [
+        { id: 1, name: "apples", quantity: 2 },
+        { id: 2, name: "bananas", quantity: 0 },
+        { id: 3, name: "cherries", quantity: 5 },
+    ];
+
+    const handleClick = (id) => {
+        const result = inventory.find((idx) => idx.id === id);
+
+        console.log(result);
+    };
+
+    return (
+        <>
+            {inventory.map((item, i) => (
+                <>
+                    <h4>{item.name}</h4>
+                    <button onClick={() => handleClick(item.id)}>Click</button>
+                </>
+            ))}
+        </>
+    );
+
+}
+
+export default App;
